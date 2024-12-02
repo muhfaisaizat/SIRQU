0064f173da9c8fcebc8cc15e35e1edec10b9754a
muhamad fais aizat <muhfaisaizat@gmail.com>
2024-12-03 06:53:59 +0700

    fectData: menyambungkan API Pajak dan biaya operasional

--- frontend/src/pages/AdminPanel/Pengaturan/Pajak&Struk/Pajak.jsx
+++ frontend/src/pages/AdminPanel/Pengaturan/Pajak&Struk/Pajak.jsx
@@ -1,16 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch } from "@/components/ui/switch";
 import { Input } from '@/components/ui/input';
+import axios from 'axios';
+import { API_URL } from "../../../../helpers/networt";
 
 const PengaturanPajak = () => {
   
+    const [dataPajak, setDataPajak] = useState(null);
     const [pajak, setPajak] = useState('');
+    const [dataBiaya, setDataBiaya] = useState(null);
     const [biaya, setBiaya] = useState('');
     const [isSwitchOn, setIsSwitchOn] = useState(false); 
     const [isSwitchOn1, setIsSwitchOn1] = useState(false); 
 
+    const fetchData = async () => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.get(`${API_URL}/api/pajaks`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+  
+            const data = response.data.data;
+
+            // Filter data untuk nama tertentu
+            const filteredDataPajak = data.filter(item => item.name === 'Pajak');
+            const filteredDataOperasional = data.filter(item => item.name === 'Biaya Operasional');
+            setIsSwitchOn(filteredDataPajak[0].status);
+            setIsSwitchOn1(filteredDataOperasional[0].status);
+            setDataPajak(filteredDataPajak[0])
+            setDataBiaya(filteredDataOperasional[0])
+            if (filteredDataPajak[0].nilaiPajak === 'null') {
+                setPajak('')
+            } else {
+                const nilaiPajak = filteredDataPajak[0].nilaiPajak.replace('%', '');
+                setPajak(nilaiPajak);
+            }
+            if (filteredDataOperasional[0].nilaiPajak === 'null') {
+                setBiaya('')
+            } else {
+                const nilaiOperasional = filteredDataOperasional[0].nilaiPajak.replace('%', '');
+                setBiaya(nilaiOperasional)
+            }
+        } catch (error) {
+            console.error("Error fetching data", error);
+        }
+    };
+    // Ambil data dari API
+    useEffect(() => {
+    
+        fetchData();
+    }, []);
+
+
+    const handleSwitchChange = async (checked) => {
+        setIsSwitchOn(checked); // Mengubah status switch
+        if (!dataPajak || dataPajak.length === 0) {
+            console.error("Data Pajak tidak ditemukan");
+            return;
+        }
+    
+        const statusValue = checked ? "true" : "false"; // Set status berdasarkan nilai switch
+        const token = localStorage.getItem("token");
+    
+        try {
+            const response = await axios.put(
+                `${API_URL}/api/pajaks/status/${dataPajak.id}?status=${statusValue}`,
+                null, // Tidak ada body yang dikirimkan
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            fetchData();
+        } catch (error) {
+            console.error("Error updating status:", error);
+        }
+    };
+
+    const handleSwitchChange1 = async (checked) => {
+        setIsSwitchOn(checked); // Mengubah status switch
+        if (!dataBiaya || dataBiaya.length === 0) {
+            console.error("Data operasional tidak ditemukan");
+            return;
+        }
+    
+        const statusValue = checked ? "true" : "false"; // Set status berdasarkan nilai switch
+        const token = localStorage.getItem("token");
+    
+        try {
+            const response = await axios.put(
+                `${API_URL}/api/pajaks/status/${dataBiaya.id}?status=${statusValue}`,
+                null, // Tidak ada body yang dikirimkan
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            fetchData();
+        } catch (error) {
+            console.error("Error updating status:", error);
+        }
+    };
+
     const formatNumber = (number) => {
-        return number.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+        return number.replace(/\B(?=(\d{3})+(?!\d))/g, '');
     };
 
     const handleInputChange = (e) => {
@@ -26,6 +123,49 @@ const PengaturanPajak = () => {
         setBiaya(numberValue);
     };
 
+
+    const handleKeyDownPajak = async (e) => {
+        if (e.key === 'Enter') {
+            const token = localStorage.getItem("token");
+            let nilaiPajak = pajak.trim() === '' ? 'null' : `${pajak}%`;
+            try {
+                const response = await axios.put(
+                    `${API_URL}/api/pajaks/nilai-pajak/${dataPajak.id}?nilaiPajak=${nilaiPajak}`,
+                    null, // Tidak ada body yang dikirimkan
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                fetchData();
+            } catch (error) {
+                console.error("Error updating nilai pajak:", error);
+            }
+        }
+    };
+
+    const handleKeyDownBiaya = async (e) => {
+        if (e.key === 'Enter') {
+            const token = localStorage.getItem("token");
+            let nilaiBiaya = biaya.trim() === '' ? 'null' : `${biaya}%`;
+            try {
+                const response = await axios.put(
+                    `${API_URL}/api/pajaks/nilai-pajak/${dataBiaya.id}?nilaiPajak=${nilaiBiaya}`,
+                    null, // Tidak ada body yang dikirimkan
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                fetchData();
+            } catch (error) {
+                console.error("Error updating nilai pajak:", error);
+            }
+        }
+    };
+
     return (
         <div className='pr-[16px] grid gap-[16px]'>
             <div className='grid gap-[16px]'>
@@ -33,7 +173,7 @@ const PengaturanPajak = () => {
                     <h1 className='text-[16px] font-semibold'>Pajak</h1>
                     <Switch 
                         checked={isSwitchOn} 
-                        onCheckedChange={(checked) => setIsSwitchOn(checked)} 
+                        onCheckedChange={handleSwitchChange}  
                     />
                 </div>
                 <div className='grid gap-[8px]'>
@@ -48,6 +188,7 @@ const PengaturanPajak = () => {
                             className="h-[36px] text-[14px] border-slate-300 pl-[12px] pr-[40px]"
                             value={formatNumber(pajak)}
                             onChange={handleInputChange}
+                            onKeyDown={handleKeyDownPajak}
                             disabled={!isSwitchOn} 
                         />
                         <span className={`absolute right-3 top-1/2 transform -translate-y-1/2 text-[14px] ${isSwitchOn ? 'text-black' : 'text-slate-400'}`}>
@@ -62,7 +203,7 @@ const PengaturanPajak = () => {
                     <h1 className='text-[16px] font-semibold'>Biaya Operasional</h1>
                     <Switch 
                         checked={isSwitchOn1} 
-                        onCheckedChange={(checked) => setIsSwitchOn1(checked)} 
+                        onCheckedChange={handleSwitchChange1} 
                     />
                 </div>
                 <div className='grid gap-[8px]'>
@@ -77,6 +218,7 @@ const PengaturanPajak = () => {
                             className="h-[36px] text-[14px] border-slate-300 pl-[12px] pr-[40px]"
                             value={formatNumber(biaya)}
                             onChange={handleInputChange1}
+                            onKeyDown={handleKeyDownBiaya}
                             disabled={!isSwitchOn1} 
                         />
                         <span className={`absolute right-3 top-1/2 transform -translate-y-1/2 text-[14px] ${isSwitchOn1 ? 'text-black' : 'text-slate-400'}`}>
