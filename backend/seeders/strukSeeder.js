1bb55eac0fa856084df6c1ebc6ad3c419f3fa375
muhamad fais aizat <muhfaisaizat@gmail.com>
2024-12-02 12:46:28 +0700

    fix:menambahkan id pada seeder struk

--- backend/seeders/strukSeeder.js
+++ backend/seeders/strukSeeder.js
@@ -6,36 +6,42 @@ module.exports = {
       'struks', // Nama tabel struks
       [
         {
+          id: 1,
           name: 'Logo',
           status: 'false', 
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: 2,
           name: 'Nama Toko',
           status: 'false',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: 3,
           name: 'Alamat',
           status: 'false',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: 4,
           name: 'Kontak',
           status: 'false',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: 5,
           name: 'Sosial Media',
           status: 'false',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: 6,
           name: 'Catatan',
           status: 'false',
           createdAt: new Date(),
